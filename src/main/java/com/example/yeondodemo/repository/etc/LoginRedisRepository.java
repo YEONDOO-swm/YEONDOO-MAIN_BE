b22676e2d17c082449a5062686461bf5db02c322
이석우 <iseog-u@iseog-uui-MacBookAir.local>
2023-11-29 21:18:12 +0900

    fix add repository annotation

--- src/main/java/com/example/yeondodemo/repository/etc/LoginRedisRepository.java
+++ src/main/java/com/example/yeondodemo/repository/etc/LoginRedisRepository.java
@@ -3,8 +3,10 @@ package com.example.yeondodemo.repository.etc;
 import com.example.yeondodemo.entity.LoginEntity;
 import com.example.yeondodemo.entity.RefreshEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
+@Repository
 public interface LoginRedisRepository extends CrudRepository<LoginEntity, String> {
 }
